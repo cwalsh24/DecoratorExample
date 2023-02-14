56674d8033f72860ad94d7a7eb41ac36aa004376
cwalsh24 <cwalsh24@ut.utm.edu>
2023-02-14 14:06:41 -0600

    Added the classes

---
+++ DecoratorPractice/DecoratorDemo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPractice
+{
+    public interface Widget
+    {
+        void draw();
+    }
+
+    public abstract class Decorator : Widget
+    {
+        Widget wid;
+        Decorator(Widget w)
+        {
+            w = wid;
+        }
+        public void draw()
+        {
+
+        }
+    }
+
+    public class TextField : Widget
+    {
+        int width;
+        int height;
+        public TextField(int w, int h) 
+        {
+            w = width;
+            h = height;
+        }
+        public void draw()
+        {
+
+        }
+
+    }
+
+    public class BorderDecorator : Widget
+    {
+        public BorderDecorator(Widget w)
+        {
+
+        }
+        public void draw()
+        {
+
+        }
+    }
+
+    public class ScrollDecorator : Widget
+    {
+        public ScrollDecorator(Widget w)
+        {
+
+        }
+        public void draw()
+        {
+
+        }
+    }
+
+    internal class DecoratorDemo
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
